948976d17f017658e843f96f83ec13dbe9b5379f
dexter9006 <52006237+dexter9006@users.noreply.github.com>
2024-01-09 21:33:14 +0100

    Update fr.lua

--- locales/fr.lua
+++ locales/fr.lua
@@ -6,9 +6,18 @@ local Translations = {
         ["process_cancelled"] = "Processus annulé..",
         ["didnt_work"] = "Cela n'a pas fonctionné..",
         ["emty_box"] = "La boîte est vide..",
+        ["not_allowed_time"] = "Tu ne peux pas faire ça à cette heure de la journée."
     },
     success = {
         ["worked"] = "Ça a marché!",
+    },
+    info = {
+        ["palert"] = "Tentative de Cambriolage de Maison",
+        ["henter"] = "~g~E~w~ - Enterer",
+        ["hleave"] = "~g~E~w~ - Sortir",
+        ["aint"] = "~g~E~w~ - ",
+        ["hsearch"] = "En train de fouiller..",
+        ["hsempty"] = "Vide..",
     }
 }
 
